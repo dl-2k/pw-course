0fbff6480c59b6575e35d4d624bb725878cbd142
DatLe <datle00100@gmail.com>
2026-01-19 15:10:35 +0700

    fix: sua lai bai ex9.js

--- tests/lesson-02/ex9.js
+++ tests/lesson-02/ex9.js
@@ -1,5 +1,13 @@
+/*
 for (let i = 100; i <= 100; i--) {
     if (i >= 12 && i <= 25) {
         console.log(i);
     }
 }
+*/
+
+// Sửa lại bài:
+for (let i = 25; i >= 12; i--) {
+    console.log(i);
+}
+
